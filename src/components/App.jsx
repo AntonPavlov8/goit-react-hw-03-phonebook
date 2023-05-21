6d3d0495b4d1b36236f5b94469cad8f2baf79ee6
Anison <barsikstd@gmail.com>
2023-05-21 10:46:30 -0500

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,33 @@
+import { useEffect } from 'react';
 import { useState } from 'react';
 import { PhoneBook } from './PhoneBook';
 
 export const App = () => {
   const [state, setState] = useState({
     contacts: [],
+    isInitialRender: true,
   });
+
+  function updateLocalStorage() {
+    if (state.isInitialRender) {
+      const storedData = localStorage.getItem('myData');
+      if (storedData) {
+        const parsedData = JSON.parse(storedData);
+        setState(() => {
+          return {
+            contacts: [...parsedData],
+            isInitialRender: false,
+          };
+        });
+      }
+    } else {
+      localStorage.setItem('myData', JSON.stringify(state.contacts));
+    }
+  }
+
+  useEffect(() => {
+    updateLocalStorage();
+  }, [state]);
 
   return (
     <div
--- src/components/PhoneBook.jsx
+++ src/components/PhoneBook.jsx
@@ -3,11 +3,7 @@ import { useRef } from 'react';
 import { Contacts } from './Contacts';
 export const PhoneBook = prop => {
   const nameInput = useRef(null);
-
-  const pattern = {
-    name: "^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$",
-    tel: '+?d{1,4}?[-.s]?(?d{1,3}?)?[-.s]?d{1,4}[-.s]?d{1,4}[-.s]?d{1,9}',
-  };
+  const telInput = useRef(null);
 
   const { state, setState } = prop;
 
@@ -16,48 +12,35 @@ export const PhoneBook = prop => {
     const form = e.currentTarget;
     const name = form.elements.name.value;
     const number = form.elements.number.value;
+
     if (
       state.contacts.filter(contact => {
         return contact.name === name;
       }).length === 0
     ) {
-      form.reset();
-      setState(prev => {
-        let newNumber;
-        switch (number.length) {
-          case 10: {
-            const string1 = number.substring(0, 3);
-            const string2 = number.substring(3, 6);
-            const string3 = number.substring(6, 10);
-            newNumber = `${string1}-${string2}-${string3}`;
-            break;
-          }
-          case 12: {
-            if (number[0] === '+') {
-              const string1 = number.substring(0, 2);
-              const string2 = number.substring(2, 5);
-              const string3 = number.substring(5, 8);
-              const string4 = number.substring(8, 12);
-              newNumber = `${string1}-${string2}-${string3}-${string4}`;
-            }
-            break;
-          }
-          default:
-            newNumber = number;
-            break;
-        }
-        return {
-          ...prev,
-          contacts: [
-            ...prev.contacts,
-            {
-              id: nanoid(),
-              name: name,
-              number: newNumber,
-            },
-          ],
-        };
-      });
+      if (
+        state.contacts.filter(contact => {
+          return contact.number === number;
+        }).length === 0
+      ) {
+        form.reset();
+        setState(prev => {
+          return {
+            ...prev,
+            contacts: [
+              ...prev.contacts,
+              {
+                id: nanoid(),
+                name: name,
+                number: number,
+              },
+            ],
+          };
+        });
+      } else {
+        alert(`Person with number ${number} is already in contacts.`);
+        telInput.current.focus();
+      }
     } else {
       alert(`${name} is already in contacts.`);
       nameInput.current.focus();
@@ -84,15 +67,16 @@ export const PhoneBook = prop => {
           ref={nameInput}
           type="text"
           name="name"
-          pattern={pattern.name}
+          pattern="^[a-zA-Zа-яА-Я]+(['-][a-zA-Zа-яА-Я ]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan."
           required
         />
         <span>Number</span>
         <input
+          ref={telInput}
           type="tel"
           name="number"
-          pattern={pattern.tel}
+          pattern="^(?:\+?\d{1,3})?\d+$"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
         />
